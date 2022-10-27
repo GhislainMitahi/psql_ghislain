9f950a8ff1facdd9fc6b0af7f87065eb737bfdf5
ghislainmitahi@gmail.com <ghislainmitahi@gmail.com>
2022-10-27 03:32:52 +0200

    add data

--- data.sql
+++ data.sql
@@ -1,5 +1,6 @@
 /* Populate database with sample data. */
 
-INSERT INTO animals (name) VALUES ('Luna');
-INSERT INTO animals (name) VALUES ('Daisy');
-INSERT INTO animals (name) VALUES ('Charlie');
+INSERT INTO animals VALUES (1,'Agumon', '2020-2-3', true, 0, 10.23);
+INSERT INTO animals VALUES (2,'Gabumon', '2018-11-15', true, 2, 8);
+INSERT INTO animals VALUES (3,'Pikachu', '2021-1-7', false, 1, 15.04);
+INSERT INTO animals VALUES (4, 'Devimon', '2017.5.12', true, 5, 11);
